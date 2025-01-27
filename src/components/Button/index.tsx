09d1c671c1e2bf6c2d0fc45aaaad002a39463184
Victor Mazzola <victormazzola2019@gmail.com>
2025-01-27 08:13:53 -0300

    add button props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './Button.module.scss';
 
-class Button extends React.Component {
-    render () {
+class Button extends React.Component  {
+    render() {
+
         return (
             <button className={style.button}>
-                Button
+                {this.props.children}
             </button>
         )
     }
