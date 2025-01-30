92e1ccd330fa64974bf3d0fb7d6aa1cae38f5586
Victor Mazzola <victormazzola2019@gmail.com>
2025-01-30 08:15:52 -0300

    add utils date.ts

--- src/components/Stopwatch/index.tsx
+++ src/components/Stopwatch/index.tsx
@@ -1,8 +1,10 @@
 import Button from "../Button";
 import Watch from "./Watch";
 import style from './Stopwatch.module.scss';
+import {timeToSeconds} from "../../common/utils/date.ts";
 
 export default function Stopwatch() {
+    console.log('converting:  ', timeToSeconds('01:01:01'));
     return (
         <div className={style.stopwatch}>
             <p className={style.title}>
